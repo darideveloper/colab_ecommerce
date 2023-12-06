5fda90d27bdf135c79c3303a0c1b1f807624c3e2
daridev <darideveloper@gmail.com>
2023-12-05 21:35:12 -0600

    start vite project

---
+++ src/components/button.jsx
@@ -0,0 +1,13 @@
+/**
+ * Basic button to a show a text
+ * @param {string} text text to show in the button
+ * @param {string} color button background color
+ * @returns {JSX.Element}
+ */
+export default function Button ({text, color}) {
+    return (
+        <button>
+            {text}
+        </button>
+    )
+}
